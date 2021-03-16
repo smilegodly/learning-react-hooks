030335b833801baa22eb78432bccf82b7cf59e54
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2021-03-16 16:40:13 -0400

    Added POST request to google translate API

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+const googleKey = "Place Google Translate API Key Here";
 
 const Convert = ({ language, text }) => {
   useEffect(() => {
-    console.log("new language or text");
+    axios.post(
+      "https://translation.googleapis.com/language/translate/v2",
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: googleKey,
+        },
+      }
+    );
   }, [language, text]);
 
   return <div />;
